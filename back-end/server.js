fe615870afdd3b23e567e822e4b055b7f6d805c3
Sam7898 <samarth.chandraiah@gmail.com>
2025-12-09 19:59:00 -0600

    remove body parser, use express.json(), fix port for railway

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import fs from "fs";
 import bcrypt from "bcryptjs";
-import bodyParser from "body-parser";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -10,10 +9,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Serve static files from all frontend folders
 app.use("/admin", express.static(path.join(__dirname, "../front-end/admin")));
